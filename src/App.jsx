97a9bc5263104213cbed4a6b1db1106310df4963
Salakh Salamov <mokhmadsalakh.salamov@gmail.com>
2024-10-03 17:51:03 +0300

    fixed link problem in gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <Routes>
+      <Routes basename='/film-project'>
         <Route path="/" element={<Home />} />
         <Route path="film/:id" element={<Film />} />
       </Routes>
