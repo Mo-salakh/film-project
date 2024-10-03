8101ee61e82406b47c4561ba9bbe6b1060f7b744
Salakh Salamov <mokhmadsalakh.salamov@gmail.com>
2024-10-03 18:04:59 +0300

    try n3 to fix page in gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <Routes basename='/film-project'>
+      <Routes basename='/film-project/'>
         <Route path="/" element={<Home />} />
         <Route path="film/:id" element={<Film />} />
       </Routes>
